2f3907e36a5321e05adc64f14adc2559b40b9774
Tom <T-Canfield@virginmedia.com>
2021-07-12 22:24:08 +0100

    Adding Readme

---
+++ dom/README.md
@@ -0,0 +1,23 @@
+User Guide
+
+click the buttons that correspond to digits of the number you wish to enter.
+
+Upon pressing any of the operators (+, -, x, /, %) the calculator will change any subsequent button presses as the second number you wish to use in the calculation.
+
+upon finishing entering the second number pushing the equals button will evaluate the calculation.
+
+the operators on the calculator are:
+
+- plus(+): adding two numbers together to get their automatically
+
+- subtract(-): used to subtract the second number entered from the first.
+
+- multiply (x): finds the product of the two Numbers
+
+- divide (/): splits the first number into parts the size of the second number.
+
+- modulus (%): finds the remainder after completing division.
+
+The calculator also features a A/C button which clears all values entered into the calculator up to the point of the button being pressed.
+
+And also features a +/- button which converts the number currently being entered into the calculator to either positive or negative.  
--- dom/events.js
+++ dom/events.js
@@ -5,7 +5,7 @@ var titleElement = document.getElementById('title')
 
 console.log(titleElement);
 
-titleElement.style.color= "red";
+titleElement.style.color = "red";
 
 // var listItems = document.getElementsByTagName('li')
 // console.log(listItems);
@@ -31,95 +31,88 @@ titleElement.style.color= "red";
 
 var buttons = document.getElementsByClassName("button");
 var display = document.getElementById("screen");
-var firstNumber= null;
+var firstNumber = null;
 
 //Operator
-var operator=null;
+var operator = null;
 
 var secondNumber = null;
 var result = null;
 
-function calculateResult(){
-  console.log(firstNumber);
-  console.log(operator);
-  console.log(secondNumber);
-  if(secondNumber != null){
-    if (operator=="+"){
-    result = parseFloat(firstNumber)+parseFloat(secondNumber);
+function calculateResult() {
+    console.log(firstNumber);
+    console.log(operator);
+    console.log(secondNumber);
+    if (secondNumber != null) {
+        if (operator == "+") {
+            result = parseFloat(firstNumber) + parseFloat(secondNumber);
+        }
+        if (operator == "-") {
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
+        }
+        if (operator == "x") {
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
+        }
+        if (operator == "/") {
+            result = parseFloat(firstNumber) / parseFloat(secondNumber);
+        }
+        if (operator == "%"){
+          result = parseFloat(firstNumber) % parseFloat(secondNumber);
+        }
     }
-    if (operator=="-"){
-      result = parseFloat(firstNumber)-parseFloat(secondNumber);
-    }
-    if (operator=="x"){
-      result = parseFloat(firstNumber)*parseFloat(secondNumber);
-    }
-    if (operator=="/"){
-      result = parseFloat(firstNumber)/parseFloat(secondNumber);
-    }
-  }
-  display.innerHTML= result;
-   firstNumber=null;
-   secondNumber=null;
-   operator=null;
+    display.innerHTML = result;
+    firstNumber = null;
+    secondNumber = null;
+    operator = null;
 }
-var firstNumberCheck=1;
+var firstNumberCheck = 1;
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click',function(){
-    var buttonValue=buttons[i].innerText
-    if(buttonValue== "+" || buttonValue == "-" ||  buttonValue == "x" ||  buttonValue == "/"){
-      operator = buttonValue;
-       if (firstNumberCheck==0){
-        firstNumber=calculateResult();
-      }
-      firstNumberCheck=0;
-    }else if (buttonValue == "A/C") {
-      firstNumber = null;
-      secondNumber = null;
-      operator = null;
-      display.innerHTML=0;
-      firstNumberCheck = 1;
-    }
-    else if (buttonValue == "+/-") {
-      if(firstNumberCheck==1){
-        if(firstNumber==null){
-          firstNumber = "-""
-        }else{
-        firstNumber= - parseFloat(firstNumber);
+    buttons[i].addEventListener('click', function() {
+        var buttonValue = buttons[i].innerText
+        if (buttonValue == "+" || buttonValue == "-" || buttonValue == "x" || buttonValue == "/" || buttonValue == "%") {
+            operator = buttonValue;
+            if (firstNumberCheck == 0) {
+                firstNumber = calculateResult();
+            }
+            firstNumberCheck = 0;
+        } else if (buttonValue == "A/C") {
+            firstNumber = null;
+            secondNumber = null;
+            operator = null;
+            display.innerHTML = 0;
+            firstNumberCheck = 1;
+        } else if (buttonValue == "+/-") {
+            if (firstNumberCheck == 1) {
+                if (firstNumber == null) {
+                    firstNumber = "-"
+                } else {
+                    firstNumber = -parseFloat(firstNumber);
+                }
+                display.innerHTML = firstNumber;
+            } else {
+                if (!secondNumber) {
+                    secondNumber = "-"
+                } else {
+                    secondNumber = -parseFloat(secondNumber);
+                }
+                display.innerHTML = secondNumber;
+            }
+        } else if (!firstNumber) {
+            firstNumber = buttonValue;
+            display.innerHTML = firstNumber;
+        } else if (firstNumberCheck == 1) {
+            firstNumber += buttonValue;
+            display.innerHTML = firstNumber;
+        } else if (!secondNumber) {
+            secondNumber = buttonValue;
+            display.innerHTML = secondNumber;
+        } else if (buttons[i].innerHTML.match("=")) {
+            buttons[i].addEventListener('click', calculateResult())
+            firstNumberCheck = 1;
+        } else if (firstNumberCheck == 0) {
+            secondNumber += buttonValue;
+            display.innerHTML = secondNumber;
         }
-        display.innerHTML=firstNumber;
-      }
-
-      else {
-        if(!secondNumber){
-          secondNumber = "-"
-        }else{
-        secondNumber = - parseFloat(secondNumber);
-      }
-      display.innerHTML=secondNumber;
-      }
-    }
-
-
-    else if (!firstNumber) {
-      firstNumber= buttonValue;
-      display.innerHTML = firstNumber;
-    }
-    else if(firstNumberCheck==1){
-      firstNumber+=buttonValue;
-      display.innerHTML = firstNumber;
-    }
-    else if (!secondNumber) {
-      secondNumber=buttonValue;
-      display.innerHTML = secondNumber;
-    }
-    else if(buttons[i].innerHTML.match("=")){
-      buttons[i].addEventListener('click',calculateResult())
-      firstNumberCheck=1;
-    }
-    else if(firstNumberCheck==0){
-      secondNumber += buttonValue;
-      display.innerHTML = secondNumber;
-    }
-  })
+    })
 }
